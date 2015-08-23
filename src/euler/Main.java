a009c72324650b632856641a2937958fae405cdd
Derek <derek@findthebrink.com>
2015-08-23 13:50:42 -0700

    solved euler 3

--- src/euler/Main.java
+++ src/euler/Main.java
@@ -1,7 +1,5 @@
 package euler;
 
-import java.util.Arrays;
-
 public class Main {
 	
 	public static boolean consoleLog(String x){
@@ -63,14 +61,74 @@ public class Main {
 		
 		return arr;
 	}
+	
 
+	
+	public static long getHighestMultiple(long num){
+		String tag = "getHighestMultiple: ";
+		long cnt = 2;
+		long res = 0;
+		
+		// only need to check up to the square root of the number
+		double bound = Math.sqrt((double)num);
+		long lbound = (long)bound;
+		
+		while (cnt < lbound){
+			int check = (int)(num % cnt);
+			
+			if (check == 0){
+				res = num / cnt;
+				System.out.println(tag + "Result of " + num + ": " + res);
+				return res;
+			} else {
+				cnt = cnt + 1;
+			}
+		}
+		System.out.println(tag + "Result of " + num + ": None Found!");
+		return res;
+	}
+	
+	public static boolean isMultiple(long num, long div){
+		// returns true if num is divisible by div evenly
+		if (num % div == 0){
+			return true;
+		}
+		return false;
+	}
+	
+	public static long getHighestPrimeMultiple(long num){
+		String tag = "getHighestPrimeMultiple: ";
+		// check only prime numbers
+		// return value of highest multiple
+		long cnt = (long)Math.floor(Math.sqrt((double)num));
+		
+		while (cnt > 2){
+			if (isPrime(cnt) && isMultiple(num, cnt)){
+				System.out.println(tag + "Result of " + num + ": " + cnt);
+				return cnt;
+			} else if (!isOdd(cnt)){
+				cnt = cnt - 1;
+			} else {
+				cnt = cnt - 2;
+				System.out.println(String.valueOf(cnt));
+			}
+		}
+		return 0;
+	}
+	
 	public static void main(String[] args) {
+		long testNum = new Long(600851475143L);
 		// TODO Auto-generated method stub
 //		System.out.println(isPrime(4));
 //		System.out.println(isPrime(7));
 //		System.out.println(isPrime(15));
-		System.out.println(Arrays.toString(getMultiples(10, 8)));
-		System.out.println(Arrays.toString(getMultiples(1520, 20)));
+//		System.out.println(Arrays.toString(getMultiples(10, 8)));
+//		System.out.println(Arrays.toString(getMultiples(1520, 20)));
+//		getHighestMultiple(10);
+//		getHighestMultiple(13195);
+//		getHighestMultiple(29);
+		getHighestPrimeMultiple(13195);
+		getHighestPrimeMultiple(testNum);
 	}
 
 }
